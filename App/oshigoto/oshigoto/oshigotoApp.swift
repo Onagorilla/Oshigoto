870c9870471dc68152f26bf8fbf1568f0ff185f4
kgmyshin <kgmyshin82@gmail.com>
2022-11-05 14:48:03 +0900

    new project

---
+++ App/oshigoto/oshigoto/oshigotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  oshigotoApp.swift
+//  oshigoto
+//
+//  Created by 釘宮愼之介 on 2022/11/05.
+//
+
+import SwiftUI
+
+@main
+struct oshigotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
